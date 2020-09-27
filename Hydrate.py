96ab677ec9674d55523834b0cd79da233f67bd21
ccomet <ccomet88@gmail.com>
2020-09-26 20:29:24 -0400

    Awakening Tofu

--- Hydrate.py
+++ Hydrate.py
@@ -6,7 +6,7 @@ from threading import Timer
 client = discord.Client()
 prefix = "?"
 
-x = datetime.today()
+'''x = datetime.today()
 y = x.replace(day=x.day, hour=20, minute=27, second=30, microsecond=0)
 delta_t = y - x
 
@@ -19,7 +19,7 @@ async def drink_water():
         await message.channel.send('Time to drink water')
 
 t = Timer(secs, drink_water)
-t.start()
+t.start()'''
 
 
 @client.event
@@ -28,6 +28,10 @@ async def on_message(message):
         return
     if str(message.author) == "Kyo#9355":
         await message.channel.send("Hello " + "<@352330857286991902>")
+    else:
+        await message.channel.send("Tofu")
+    if str(message.author) == "Xenon#3636":
+        await message.channel.send("Hello " + "<@528299195325480960>")
     else:
         await message.channel.send("Tofu")
 
