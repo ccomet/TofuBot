0605d952eb4ab7db33551dc55f8bdcb80d7621a8
ccomet <ccomet88@gmail.com>
2020-09-27 14:19:08 -0400

    Awakening Tofu

--- __main__.py
+++ __main__.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from threading import Timer
 
 client = discord.Client()
-prefix = "tofu"
+prefix = "tofu "
 
 
 # Hello Response
@@ -16,11 +16,8 @@ async def on_message(message):
     if message.content.startswith(prefix + "hello"):
         msg = 'Hello {0.author.mention}'.format(message)
         await message.channel.send(msg)
-    if message.content.startswith("hello"):
-        if str(message.author) == "Kyo#9355":
-            await message.channel.send("Hello " + "<@352330857286991902>")
-        if str(message.author) == "Xenon#3636":
-            await message.channel.send("Hello " + "<@528299195325480960>")
+    elif message.content.startswith(prefix + "ping"):
+        await message.channel.send("pong")
 
 
 client.run(os.environ['DISCORD_TOKEN'])
