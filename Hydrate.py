5a0909a726e701f58d3827ed38bca87a597fc29b
ccomet <ccomet88@gmail.com>
2020-09-26 20:05:56 -0400

    Awakening Tofu

--- Hydrate.py
+++ Hydrate.py
@@ -5,11 +5,20 @@ import datetime
 client = discord.Client()
 prefix = "?"
 
-x=datetime.today()
-y=x.replace(day=x.day, hour=20, minute=3, second=30, microsecond=0)
-delta_t=y-x
+x = datetime.today()
+y = x.replace(day=x.day, hour=20, minute=6, second=30, microsecond=0)
+delta_t = y-x
 
-secs=delta_t.seconds+1
+secs = delta_t.seconds+1
+
+@client.event
+async def drink_water():
+    await message.channel.send("Time to drink water")
+
+t = Timer(secs, drink_water)
+t.start()
+
+
 @client.event
 async def on_message(message):
     if message.author == client.user:
